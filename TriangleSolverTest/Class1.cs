8044c13ab99cad41abc88f5e731717e7670ada4b
unknown <jainanant36@gmail.com>
2023-06-08 15:01:02 -0400

    Zero and non zero test cases

--- TriangleSolverTest/Class1.cs
+++ TriangleSolverTest/Class1.cs
@@ -21,5 +21,71 @@ namespace TriangleSolverTest
             String output = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
             Assert.AreEqual("The triangle is valid and is an EQUILATERAL", output);
         }
+
+        [Test]
+        public static void VerifySideAToNotAcceptZeroInput0and3and3OutputInvalidTriangle()
+        {
+            int sideA = 0;
+            int sideB = 3;
+            int sideC = 3;
+
+            String output = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", output);
+        }
+
+        [Test]
+        public static void VerifySideBToNotAcceptZeroInput3and0and3OutputInvalidTriangle()
+        {
+            int sideA = 3;
+            int sideB = 0;
+            int sideC = 3;
+
+            String output = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", output);
+        }
+
+        [Test]
+        public static void VerifySideCToNotAcceptZeroInput3and3and0OutputInvalidTriangle()
+        {
+            int sideA = 3;
+            int sideB = 3;
+            int sideC = 0;
+
+            String output = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", output);
+        }
+
+        [Test]
+        public static void VerifySideAForInvalidResponseInputNegative1and3and3OutputInvalidTriangle()
+        {
+            int sideA = -1;
+            int sideB = 3;
+            int sideC = 3;
+
+            String output = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            Assert.AreEqual("A triangle cannot be formed with those numbers", output);
+        }
+
+        [Test]
+        public static void VerifySideAForInvalidResponseInput3andNegative1and3OutputInvalidTriangle()
+        {
+            int sideA = 3;
+            int sideB = -1;
+            int sideC = 3;
+
+            String output = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            Assert.AreEqual("A triangle cannot be formed with those numbers", output);
+        }
+
+        [Test]
+        public static void VerifySideAForInvalidResponseInput3and3andNegative1OutputInvalidTriangle()
+        {
+            int sideA = 3;
+            int sideB = 3;
+            int sideC = -1;
+
+            String output = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+            Assert.AreEqual("A triangle cannot be formed with those numbers", output);
+        }
     }
 }
